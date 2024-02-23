3eb89f308e02aa24e83356fb0a5e3e84da9d13ad
Daigoh <diego.valdes14@gmail.com>
2024-02-23 09:41:56 -0300

    seed file and images, padding product grid

--- src/components/products/product-grid/ProductGrid.jsx
+++ src/components/products/product-grid/ProductGrid.jsx
@@ -2,7 +2,7 @@ import { ProductGridItem } from "./ProductGridItem"
 
 export const ProductGrid = ({products}) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 mb-10 px-0 sm:px-8">
         {
             products.map( product => (
                 <ProductGridItem key={product.slug} product={product}/>
