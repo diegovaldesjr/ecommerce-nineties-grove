f078595e4c26f5c7b994639193780db00a7f0dd0
Daigoh <diego.valdes14@gmail.com>
2024-03-05 20:21:42 -0300

    expand session object

--- src/auth.config.js
+++ src/auth.config.js
@@ -33,6 +33,18 @@ export const authConfig = {
     signIn: 'auth/login',
     newUser: 'auth/new-account'
   },
+  callbacks: {
+    jwt({token, user}) {
+      if (user) {
+        token.data = user
+      }
+      return token
+    },
+    session({session, token, user}) {
+      session.user = token.data
+      return session
+    }
+  },
   providers: [
     Credentials({
       async authorize(credentials) {
